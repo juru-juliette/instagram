fa44d0915df4a3ea99ef5c86336206c4fb72ba19
juru-juliette <kangabejuliette@gmail.com>
2019-10-18 10:53:34 +0200

    creating profile template

--- insta/urls.py
+++ insta/urls.py
@@ -4,7 +4,8 @@ from django.conf.urls import url
 from . import views
 urlpatterns=[
     url('^$',views.home,name = 'home'),
-    # url(r'^search/', views.search_results, name='search_results'),
+    url(r'^profile/', views.profile, name='profile'),
+   
 ]
 ## this references the location to the uploaded files.
 if settings.DEBUG:

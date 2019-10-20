bc0e854e0e762084ecf31d533b9229c6088522b5
juru-juliette <kangabejuliette@gmail.com>
2019-10-20 22:49:59 +0200

    editing navbar

--- insta/urls.py
+++ insta/urls.py
@@ -6,8 +6,8 @@ urlpatterns=[
     url('^$',views.home,name = 'home'),
     url(r'^new/post$', views.new_post, name='new-post'),
     url(r'^edit_profile/(\d+)',views.edit_profile,name ='edit_profile') ,
-    url(r'^profile/(\d+)',views.profile,name ='profile') ,
-    url(r'^search',views.search_results,name = 'search')
+    url(r'^profile/(\d+)',views.profile,name ='profile'),
+    url(r'^search/', views.search_results, name='search_results'),
    
 ]
 ## this references the location to the uploaded files.
